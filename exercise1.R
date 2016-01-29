445a2e5e7c9675734a1610f2301ea0a3687baef1
Kush Gupta <bluek7@D-173-250-237-193.dhcp4.washington.edu>
2016-01-29 15:16:07 -0800

    Whatever

--- exercise1.R
+++ exercise1.R
@@ -1,43 +1,45 @@
 ### Exercise 1 ###
 
 # Install the nycflights13 package and read it in.  Require the dplyr package
-# install.packages("nycflights13")
+install.packages("nycflights13")
 library(nycflights13)
 library(dplyr)
 
 # The data.frame flights should now be accessible to you.  View it, 
 # and get some basic information about the number of rows/columns
-
+View(flights)
 
 # Add a column that is the amount of time gained in the air
-
+flights <- mutate(flights, gain = arr_time - dep_time)
 
 # Sort your data.frame desceding by the column you just created
-
+arrange(flights, desc(gain))
 
 # Try doing the last 2 steps in a single operation using the pipe operator
-
+flights <- mutate(flights, gain = dep_time - arr_time) %>% arrange(desc(gain))
 
 # Make a histogram of the amount of gain using the `hist` command
-
+hist(flights$gain)
 
 # On average, did flights gain or lose time?
-
+flights[['gain']] %>% mean(na.rm = TRUE)
 
 # Create a data.frame of flights headed to seatac ('SEA'), 
 # and only include the column you just created
+seatac <- flights %>% filter(dest == 'SEA') %>% mutate()
 
-
-# On average, did flights to seatac gain or loose time?
-
+# On average, did flights to seatac gain or lose time?
+seatac[['gain']] %>% mean(na.rm = TRUE)
 
 # Write a function that allows you to specify an origin, a destination, and a column of interest
 # that returns a data.frame of flights from the origin to the destination and only the column of interest
 ## Hint: see slides on standard evaluation
-
+interest <- function(start, destination, column) {
+  return (flights %>% filter(origin == start, destination == dest) %>% select_(column))
+}
 
 # Retireve the air_time column for flights from JFK to SEA
-
+jkfsea <- interest('JFK', 'SEA', 'air_time')
 
 # What was the average air time of those flights (in hours)?  What was the min/max?
 
--- exercise2.R
+++ exercise2.R
@@ -7,15 +7,23 @@ library(dplyr)
 
 # In which month was the average departure delay the greatest?
 # Hint: you'll have to perform a grouping operation before summarizing your data
-
+flights %>% group_by(month) %>%
+            summarise(avg = mean(dep_delay, na.rm = TRUE)) %>%
+            filter(avg == min(avg))
 
 # If you create a data.frame with the columns "month", and "delay" above, you should be able to create 
 # a scatterplot by passing it to the 'plot' function
-
+flights %>% group_by(month) %>%
+            summarise(delay = mean(dep_delay, na.rm = TRUE)) %>% plot()
+              
+            
 
 # In which airport were the average arrival delays the highest?
 # Hint: you'll have to perform a grouping operation before summarizing your data
+flights %>% group_by(dest) %>%
+              summarise(avg = mean(arr_delay, na.rm = TRUE))
 
+  filter(delays, avg == max(avg, na.rm = TRUE))
 
 ### Bonus ###
 # Which city was flown to with the highest average speed?
